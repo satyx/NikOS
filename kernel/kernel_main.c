aad9d77cfd13b2b425be7840ac6f0bf1b1327ac1
satyx <satyamcrj@gmail.com>
2020-12-18 21:57:23 +0530

    GDT

--- kernel/kernel_main.c
+++ kernel/kernel_main.c
@@ -1,8 +1,11 @@
 #include <screen.h>
 #include <stdio.h>
+#include <gdt.h>
+
 int kmain(void *arg)
 {
 	set_color(2,15);
+	init_gdt();
 	clear_screen();
 	char *nikhil = "Nikhil Sharma\0";
 	int p = printf("In the Beloved Memory of\n%s\n",nikhil);
